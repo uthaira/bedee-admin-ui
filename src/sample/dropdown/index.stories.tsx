b78aba3d8f4253e068b6546a837b22fcc0228586
Pakorn Sukmankong <pakorn.su@BDMS.co.th>
2024-03-28 17:15:39 +0700

    feat: dropdown component

---
+++ src/sample/dropdown/index.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta } from '@storybook/react'
+import { Dropdown, RadioGroup } from '../../components'
+import { useState } from 'react'
+import { Box } from '@mui/material'
+
+export const SampleDropdown = () => {
+  const mockData = [
+    { label: 'One', value: 1 },
+    { label: 'Two', value: 2 },
+    { label: 'Three', value: 3 },
+  ]
+
+  const [selected, setSelected] = useState('')
+
+  const onSelectChange = (event:any) => setSelected(event.target.value) 
+
+  return (
+    <Box>
+      <Dropdown titleLabel="Dropdown" data={mockData} value={selected} onChange={onSelectChange} placeholder="Select Item" height="36px"/>
+    </Box>
+  )
+}
+
+export default {
+  title: 'Sample/Dropdown',
+  component: SampleDropdown,
+} as Meta<typeof SampleDropdown>
