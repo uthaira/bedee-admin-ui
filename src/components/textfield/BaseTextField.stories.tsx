940ade6edaa432112dd3a5ca6b2eeda31a42a137
Pakorn Sukmankong <pakorn.su@BDMS.co.th>
2024-04-02 21:25:38 +0700

    feat: input component

---
+++ src/components/textfield/BaseTextField.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react'
+import TextField from './BaseTextField'
+
+export default {
+  title: 'Components/TextField',
+  component: TextField,
+} as Meta<typeof TextField>
+
+type TextFieldStory = StoryObj<typeof TextField>
+
+export const BaseRadio: TextFieldStory = {
+  args: {
+    titleLabel: 'Title Label',
+    value: '',
+    placeholder: 'Placeholder',
+    disabled: false,
+    error: false,
+    multiline: false,
+    rows: 4,
+    maxRows: 4,
+  },
+}
---
+++ src/sample/textfield/index.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta } from '@storybook/react'
+import { TextField } from '../../components'
+import { Stack } from '@mui/material'
+import { useState } from 'react'
+
+export const SampleTextField = () => {
+  const [text, setText] = useState('')
+
+  const onChangeInput = (value: any) => {
+    setText(value)
+  }
+
+  return (
+    <Stack gap={2}>
+      <TextField value={text} bdOnChange={onChangeInput} titleLabel="Text Field" placeholder="placeholder" />
+      <TextField value={text} bdOnChange={onChangeInput} titleLabel="Text Field - disabled" disabled placeholder="placeholder" />
+      <TextField value={text} bdOnChange={onChangeInput} titleLabel="Text Field - error" error placeholder="placeholder" />
+      <TextField value={text} bdOnChange={onChangeInput} titleLabel="Text Area" multiline rows={4} maxRows={4} placeholder="placeholder" />
+    </Stack>
+  )
+}
+
+export default {
+  title: 'Sample/TextField',
+  component: SampleTextField,
+} as Meta<typeof SampleTextField>
